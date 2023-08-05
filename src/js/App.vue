d348d7e66830491a2937ca309166531caf219118
djneskovic <djneskovic@gmail.com>
2023-08-05 20:47:38 +0200

    New portfolio

---
+++ src/js/App.vue
@@ -0,0 +1,3 @@
+<template>
+	<div><p>odlicno</p></div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "/src/js/App.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
